87eee73ba0088aac71d7834ea40cfe7facc89b74
suragch <suragch@suragch.dev>
2026-05-14 14:20:50 +0800

    location permission

--- lib/ui/demos/12_permissions/permissions_demo.dart
+++ lib/ui/demos/12_permissions/permissions_demo.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class PermissionsDemo extends StatefulWidget {
   const PermissionsDemo({super.key});
@@ -8,6 +9,57 @@ class PermissionsDemo extends StatefulWidget {
 }
 
 class _PermissionsDemoState extends State<PermissionsDemo> {
+  String _message = '';
+
+  Future<void> _getCurrentLocation() async {
+    bool serviceEnabled;
+
+    // Step 1: Check if GPS is turned on
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      setState(() {
+        _message = 'GPS is not enabled. Please enable it and try again.';
+      });
+      return;
+    }
+
+    // Step 2: Check if the the app has permission to use GPS
+    var permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      // ask the user for permission
+      permission = await Geolocator.requestPermission();
+
+      if (permission == LocationPermission.denied) {
+        setState(() {
+          _message = 'GPS permission was denied.';
+        });
+        return;
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      setState(() {
+        _message =
+            'GPS permission is permanently denied. Please enable it in your phone settings.';
+      });
+      return;
+    }
+
+    // Step 3: If we have permission, get the GPS coordinates
+    setState(() {
+      _message = 'Fetching coordinates...';
+    });
+
+    final position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.high,
+    );
+
+    setState(() {
+      _message =
+          'Latitude: ${position.latitude}\nLongitude: ${position.longitude}';
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,9 +68,12 @@ class _PermissionsDemoState extends State<PermissionsDemo> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Hello world', style: TextStyle(fontSize: 30)),
+            Text(_message, style: TextStyle(fontSize: 30)),
             SizedBox(height: 20),
-            ElevatedButton(onPressed: () {}, child: Text('Find my location')),
+            ElevatedButton(
+              onPressed: _getCurrentLocation,
+              child: Text('Find my location'),
+            ),
           ],
         ),
       ),
